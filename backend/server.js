d603d02bdfee2bb48e998084a34f1e20fd4c7cd3
shivansh-bhatnagar18 <shivansh111sid@gmail.com>
2024-12-21 14:39:20 +0530

    Added GAuth

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,10 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+// Routes
+app.use('/api/users', require('./routes/admin'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/grievances', require('./routes/grievance'));
 
 // Start the server
 app.listen(PORT, () => {
