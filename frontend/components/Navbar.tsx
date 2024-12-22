951ef1f0c66426b99f5473f67b748b19aa13e08c
PrathamX595 <pratham.seth.cer23@itbhu.ac.in>
2024-12-22 18:46:53 +0530

    changed nav color

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -7,7 +7,7 @@ import Logo from "@/public/IITBHU_LOGO.png";
 function Navbar() {
   return (
     <nav>
-      <div className="flex justify-between items-center bg-[#106ea8] p-2 shadow-lg">
+      <div className="flex justify-between items-center bg-[#703f6c] p-2 shadow-lg">
           <Link href="/" className="flex items-center gap-3 ml-6">
             <Image
               src={Logo}
@@ -20,7 +20,7 @@ function Navbar() {
         <div className="mr-6">
           <a
             href="/grievancePage"
-            className="text-white bg-[#703f6c] hover:bg-[#50274a] py-2 px-4 rounded-lg font-semibold transition duration-300 shadow-md"
+            className="text-white bg-[#106ea8] hover:bg-[#2878a9] py-2 px-4 rounded-lg font-semibold transition duration-300 shadow-md"
           >
             Submit Grievances
           </a>
