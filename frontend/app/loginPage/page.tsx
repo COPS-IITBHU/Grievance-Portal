f18abc64fef88224a1c92799e28feb46d78ed280
shivansh-bhatnagar18 <shivansh111sid@gmail.com>
2025-01-07 00:04:53 +0530

    Minor changes vefore deplomeent

--- frontend/app/loginPage/page.tsx
+++ frontend/app/loginPage/page.tsx
@@ -3,8 +3,9 @@
 import React, { useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation'
 import { authService } from '@/services/api';
+import { Suspense } from 'react';
 
-function LoginPage() {
+function LoginContent() {
   const router = useRouter();
   const searchParams = useSearchParams();
 
@@ -51,6 +52,14 @@ function LoginPage() {
         </button>
       </div>
     </div>
+  );
+}
+
+function LoginPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginContent />
+    </Suspense>
   );
 }
 
