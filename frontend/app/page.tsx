71fcc80b46497fe2d94325908ddfb98b787f70f2
shivansh-bhatnagar18 <shivansh111sid@gmail.com>
2025-01-22 16:25:31 +0530

    Frontend Finalizing

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,9 @@
 import HomePage from "./homePage/page";
+import ProfilePage from "./ProfilePage/page";
 export default function Home() {
   return (
     <div>
-      <HomePage />
+      <ProfilePage />
     </div>
   );
 }
